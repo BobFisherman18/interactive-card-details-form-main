3a072243f0024970e604967e581159c4ae5bfa67
BobFisherman18 <jonahrivera2010@hotmail.com>
2024-09-10 21:55:32 -0700

    added a useRef Object for inputs

--- src/inputs.jsx
+++ src/inputs.jsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect, useContext, useRef, forwardRef } from "reac
 
 export function Inputs() {
 
-const nameRef = useRef(0);
+    const [isThereError, setIsThereError] = useState(false);
+
+    const inputRefs = {
+        name: useRef(),
+        number: useRef(),
+        month: useRef(),
+        year: useRef(),
+        cvc: useRef()
+    }
 
 
     // Function to handle input change
     const handleInputChange = (e) => {
-
             console.log(e.target.value);
+            
             
         };
     useEffect(() => {
@@ -16,25 +24,33 @@ const nameRef = useRef(0);
     })
     //Function to handle button click
     const handleButtonClick = () => {
-        nameRef.current ++;
+        const inputs = Object.values(inputRefs);
+        inputs.map(input => {
+            console.log(input.current);
+            input.current.value ===`` ? setIsThereError(true) :
+                                        setIsThereError(false)
+
+        });
     };
     
     return (
         <section id="inputs" className="mx-4">
-            <InputField 
-            >
+            <InputField >
                 <Label htmlFor='name'>CARDHOLDER NAME</Label>
                 <Input 
                     type="text"
                     placeholder="e.g. Jane Appleseed"
                     onChange={handleInputChange}
+                    ref={inputRefs.name}
+                    errorState={isThereError === true ? `errorState` : ``}
                 />
             </InputField>
             <InputField>
                 <Label htmlFor='num'>CARD NUMBER</Label>
                 <Input 
                   type="number"
-                  placeholder="e.g. 1234 5678 9123 0000"  
+                  placeholder="e.g. 1234 5678 9123 0000"
+                  ref={inputRefs.number}  
                 />
             </InputField>
             <InputField>
@@ -47,11 +63,13 @@ const nameRef = useRef(0);
                                 placeholder="MM"
                                 col='col'
                                 margin='me-2'
+                                ref={inputRefs.month}
                             />
                             <Input 
                                 type='number'
                                 placeholder="YY"
                                 col='col'
+                                ref={inputRefs.year}
                             />
                         </InputField>
                     </InputField>
@@ -60,23 +78,25 @@ const nameRef = useRef(0);
                         <Input 
                             type='number'
                             placeholder='e.g. 123'
+                            ref={inputRefs.cvc}
                         />
                     </InputField>
                 </InputField>
             </InputField>
-            <div className="d-grid">
-            <button className="btn my-3" onClick={handleButtonClick}>
-                Confirm
-            </button>
-            </div>
+            <InputField className="d-grid">
+                <Button margin='my-3'onClick={handleButtonClick}>
+                    Confirm   
+                </Button> 
+            </InputField>
         </section>
     );
 }
-
-export function InputField({
-    className='mb-3',
-    children
-    }) {
+export function Button({ margin, onClick, children }) {
+    return (
+        <button className={`btn ${margin}`} onClick={onClick}>{children}</button>
+    );
+}
+export function InputField({ className='mb-3', children }) {
     return (
     <>
         <div className={className}>
@@ -85,25 +105,23 @@ export function InputField({
     </>
     );
 }
-export function Input({
-    col=``,
-    margin=``,
-    type,
-    placeholder,
-    onChange,
-    }) {
-    return (
-        <input 
-            type={type} 
-            className={`form-control ${col} ${margin}`} 
-            placeholder={placeholder}   
-            onChange={onChange}/>
-    );
-}
-export function Label({
-    htmlFor,
-    children
-    }) {
+//This component is used to forward refs as props
+const Input = forwardRef(( props, ref ) => {
+        return (
+            <>
+            <input 
+                type={props.type} 
+                className={`form-control ${props.errorState} ${props.col} ${props.margin}`} 
+                placeholder={props.placeholder}   
+                onChange={props.onChange}
+                ref={ref}
+            /> 
+            </>
+        )
+    });
+export default Input;
+
+export function Label({ htmlFor, children }) {
     return (
         <label htmlFor={htmlFor} className='form-label'>{children}</label>
     );
